f265a314f2db678dec88e6e3b9f84840b401dadd
Luc-Veldhuis <luc.veldhuis@nubisonline.nl>
2020-08-10 11:36:39 +0200

    Fix paths

--- angora_results/run_size.sh
+++ angora_results/run_size.sh
@@ -1,2 +1,2 @@
 cd ../fuzz_checker
-pipenv run python ./executor.py -b ../experiments/bin/size -c ../experiments/sym/bin/size -t ../angora_results/size/output_test/ -o ../angora_results/size/results_test/
+pipenv run python ./executor.py -b ../experiments/binutils/bin/size -c ../experiments/sym/binutils/bin/size -t ../angora_results/size/output/traces/ -o ../angora_results/size/results/
