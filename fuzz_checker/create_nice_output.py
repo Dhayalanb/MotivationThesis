dcde946978fcc39fb39344bbbbf7156c7dd7f197
Luc Veldhuis <laserluc@live.nl>
2020-11-20 12:41:02 +0100

    Commit output parser

---
+++ fuzz_checker/create_nice_output.py
@@ -0,0 +1,145 @@
+import sys, getopt
+from collections import Counter
+import scipy.stats
+import matplotlib.pyplot as plt
+
+STRATEGIES = ['GradientDescentStrategy', 'MagicByteStrategy' , 'OneByteStrategy', 'LengthTaintStrategy', 'ConcolicStrategy', 'RandomStrategy', 'RandomTaintStrategy']
+VARIABLES_TO_TEST = ['oviedo', 'cyclomatic' , 'nrOfOffsets', 'cases', 'chain_size', 'depth2', 'trace_length']
+LOCATION = '../results/matplots/'
+
+def make_tests(header, data):
+    test_results = {}
+    for strategy in STRATEGIES:
+        data_to_test = data[strategy]
+        for variable in VARIABLES_TO_TEST:
+            #collect data
+            collected_data = []
+            for i in range(len(data_to_test[variable])):
+                if data_to_test[variable][i] == '-':
+                    continue
+                if variable == 'trace_length':
+                    collected_data.append((data_to_test['depth2'][i]/data_to_test['trace_length'][i], data_to_test['flipped'][i]))
+                else:
+                    collected_data.append((data_to_test[variable][i], data_to_test['flipped'][i]))
+            #Split in 2 groups, 1 population with flipped, 1 without flipped an see if it comes from 2 different populations
+            x = [x[0] for x in collected_data if x[1] == 0] #population which is not flipped
+            y = [x[0] for x in collected_data if x[1] == 1] #population which is flipped
+            print("testing %s" % (variable))
+            test_results[variable] = scipy.stats.mannwhitneyu(x, y, use_continuity = (True if variable == 'relative_depth' else False))
+    return test_results
+
+def make_plots(header, data, test_result, name):
+    #make total flip plot
+    keys = {}
+    for strategy in STRATEGIES:
+        keys[strategy] = Counter(data[strategy]['status'])
+    all_statuses = list(set([status for key in keys for status in keys[key]]))
+    labels = STRATEGIES
+    all_values = []
+    for status in all_statuses:
+        values = []
+        for strategy in labels:
+            if status in keys[strategy]:
+                values.append(keys[strategy][status])
+            else:
+                values.append(0)
+        all_values.append(values)
+    bottom = [0 for _ in range(len(all_values[0]))]
+    plt.figure(1)
+    fig, ax = plt.subplots(figsize=(7, 7))
+    for i in range(len(all_values)):
+        ax.bar(labels, all_values[i], label=all_statuses[i], width = 0.5, bottom=bottom)
+        bottom = [bottom[j] + all_values[i][j] for j in range(len(all_values[i]))]
+    ax.legend()
+    plt.xticks(rotation=90)
+    plt.subplots_adjust(bottom=0.4)
+    plt.savefig(LOCATION + name + '_total_flips.svg')
+    #make substrategy flip plot
+    #TODO
+    #make absolute depth plot
+    plt.figure(3)
+    fig, ax = plt.subplots(figsize=(7, 7))
+    labels = ["%s-%s%%" % (i,i+10) for i in range(0,100,10)]
+    for strategy in STRATEGIES:
+        data_to_bucket = [(data[strategy]['depth2'][i], data[strategy]['flipped'][i]) for i in range(len(data[strategy]['depth2']))]
+        max_value = float(max([value[0] for value in data_to_bucket]))
+        x = [sum([value[1] for value in data_to_bucket if value[0] >= max_value*i/10 and (value[0] < (i+1)*max_value/10  or (i == 9 and value[0] <= (i+1)*max_value/10))]) for i in range(0,10)]
+        ax.plot(labels, x, label=strategy)
+    ax.legend()
+    plt.savefig(LOCATION + name + '_absolute_depth.svg')
+    #make relative depth plot
+    plt.figure(4)
+    fig, ax = plt.subplots(figsize=(7, 7))
+    labels = ["%s-%s%%" % (i,i+10) for i in range(0,100,10)]
+    for strategy in STRATEGIES:
+        data_to_bucket = [(data[strategy]['depth2'][i]*100./data[strategy]['trace_length'][i], data[strategy]['flipped'][i]) for i in range(len(data[strategy]['depth2']))]
+        x = [sum([value[1] for value in data_to_bucket if value[0] >= i and (value[0] < (i+10)  or (i == 90 and value[0] <= (i+10)))]) for i in range(0,100,10)]
+        ax.plot(labels, x, label=strategy)
+    ax.legend()
+    plt.savefig(LOCATION + name + '_relative_depth.svg')
+    #make offset plot
+    plt.figure(5)
+    fig, ax = plt.subplots(figsize=(7, 7))
+    for strategy in STRATEGIES:
+        data_to_show = [(data[strategy]['nrOfOffsets'][i], data[strategy]['flipped'][i]) for i in range(len(data[strategy]['nrOfOffsets']))]
+        labels = list(set([value[0] for value in data_to_show]))
+        labels.sort()
+        x = [sum([value [1] for value in data_to_show if value[0] == label]) for label in labels]
+        labels = [str(x) for x in labels]
+        ax.plot(labels, x, label=strategy)
+    ax.legend()
+    plt.savefig(LOCATION + name + '_offsets.svg')
+    #make statistic test table
+    
+    #make time table
+
+def process_file(raw_file):
+    with open(raw_file, 'r') as input_file:
+        data = input_file.read()
+    processed_data = {}
+    header = data.split('\n')[0].split(',')
+    for row in data.split('\n')[1:]:
+        columns = row.split(',')
+        if len(columns) != len(header):
+            continue
+        strategy = columns[0]
+        if strategy not in processed_data:
+            processed_data[strategy] = {}
+        for i in range(len(header)):
+            column_name = header[i]
+            if column_name not in processed_data[strategy]:
+                processed_data[strategy][column_name] = []
+            value = columns[i]
+            try:
+                value = int(value)
+            except ValueError:
+                pass
+            processed_data[strategy][column_name].append(value)
+    return (header, processed_data)
+
+def main(argv):
+    raw_file = ""
+    name = ''
+    try:
+        opts, args = getopt.getopt(argv,"r:n:",["raw=", "name="])
+    except getopt.GetoptError:
+        print('create_nice_output.py -r <raw> -n <name>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('create_nice_output.py -r <raw> -n <name>')
+            sys.exit()
+        elif opt in ("-r", "--raw"):
+            raw_file = arg
+        elif opt in ("-n", "--name"):
+            name = arg
+    print("Processing data...")
+    header, processed_data = process_file(raw_file)
+    print("Doing tests")
+    test_result = make_tests(header, processed_data)
+    print("Making plots")
+    make_plots(header, processed_data, test_result, name)
+    print("Done!")
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
---
+++ fuzz_checker/create_raw_output.py
@@ -0,0 +1,98 @@
+from static_parser import StaticParser
+from output_parser import Parser
+from importer import Importer
+import getopt, sys
+
+def combine_results(dynamic_files, static_files, relative_depth):
+    results = []
+    strategies = list(dynamic_files.keys())
+    strategies.sort()
+    del strategies[strategies.index("GradientDescentStrategy")]
+    strategies.insert(0, "GradientDescentStrategy")
+    offset_map = {}
+    for strategy in strategies:
+        for file_name in dynamic_files[strategy]:
+            print(file_name)
+            result = dynamic_files[strategy][file_name].copy()
+            if file_name not in offset_map:
+                offset_map[file_name] = len(result['offsets'])
+            cmpid = file_name.split('_')[0]
+            result['flipped'] = 1 if result['status'] == 'FLIPPED_CONDITION' else 0
+            result['id'] = file_name[:-5]
+            result['cmpid'] = cmpid
+            result['strategy'] = strategy
+            result['nrOfOffsets'] = offset_map[file_name]
+            should_fill_in = True if cmpid in static_files else False
+            result['cyclomatic'] = static_files[cmpid].cyclomatic if should_fill_in else "-"
+            result['oviedo'] =  static_files[cmpid].oviedo  if should_fill_in else "-"
+            result['chain_size'] =  static_files[cmpid].chain_size  if should_fill_in else "-"
+            result['cases'] =  static_files[cmpid].cases  if should_fill_in else "-"
+            if file_name[:-5] in relative_depth:
+                result['trace_length'] = relative_depth[file_name[:-5]]
+            else:
+                result['trace_length'] = -1
+            results.append(result)
+    return results
+
+
+def write_results(results, output_name):
+    csv = "Strategy,id,cmpid,nrOfMisses,nrOfInputs,depth,status,totalTime,nrOfOffsets,cyclomatic,oviedo,chain_size,cases,depth2,trace_length,flipped\n"
+    for result in results:
+        for key in result:
+            if key != 'trace_length':
+                result[key] = str(result[key])
+        csv += result['strategy'] + "," + result['id'] + "," + result['cmpid'] + "," + result['nrOfMisses'] + "," + result['nrOfInputs'] + "," + result['depth'] + "," + result['status'] + "," + result['totalTime'] + "," + result['nrOfOffsets'] + "," + result['cyclomatic'] + "," + result['oviedo'] + "," + result['chain_size'] + ","+ result['cases']+ ","+ str(result['trace_length'][0]) + ","+ str(result['trace_length'][1]) + ","+ result['flipped']
+        csv += "\n"
+    with open(output_name, 'w') as output_file:
+        output_file.write(csv)
+
+def get_depth_from_traces(traces):
+    relative_depth = {}
+    for trace in traces:
+        trace_length = trace.getConditionLength()
+        for i in range(trace_length):
+            condition = trace.getCondition(i).base
+            cmpid = condition.getLogId()
+            if cmpid not in relative_depth:
+                relative_depth[cmpid] = (i, trace_length)
+    return relative_depth
+
+def main(argv):
+    dynamic_folder = ""
+    static_folder = ""
+    output_name = ""
+    traces_folder = ""
+    try:
+        opts, args = getopt.getopt(argv,"d:s:o:t:",["dynamic=", "static=", "output=", "traces="])
+    except getopt.GetoptError:
+        print('create_raw_output.py -d <dynamic> -s <static> -t <traces>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('create_raw_output.py -d <dynamic> -s <static> -t <traces>')
+            sys.exit()
+        elif opt in ("-d", "--dynamic"):
+            dynamic_folder = arg
+        elif opt in ("-s", "--static"):
+            static_folder = arg
+        elif opt in ("-o", "--output"):
+            output_name = arg
+        elif opt in ("-t", "--traces"):
+            traces_folder = arg
+    print("Reading files...")
+    static_files = StaticParser.parse_analysis_files(static_folder)
+    print("Parsed static files")
+    dynamic_files = Parser.parse_folder(dynamic_folder)
+    print("Parsed dynamic files")
+    i = Importer(traces_folder)
+    traces = i.get_traces_iterator()
+    relative_depth = get_depth_from_traces(traces)
+    print("Parsed trace files")
+    results = combine_results(dynamic_files, static_files, relative_depth)
+    print("Created results")
+    write_results(results, output_name)
+    print("Done! Results written to" + output_name)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
