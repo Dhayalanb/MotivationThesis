fe1fb339394de1b0ea6be34abe870b449ac03290
Luc-Veldhuis <laserluc@live.nl>
2021-02-06 19:54:20 +0000

    Update result generation

--- results/generate_results.sh
+++ results/generate_results.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 
-for program in file djpeg jhead gif2png nm xmlwf 
+for program in file djpeg jhead gif2png nm xmlwf tcpdump 
 do
     echo "Processing $program"
     cd ../fuzz_checker
-    pipenv run python ./output_parser.py -i ../angora_results/$program/results/ -o ../results/$program/
+    pipenv run python ./create_raw_output.py -t ../angora_results/$program/output/traces/ -s ../results/static/$program/ -d ../angora_results/$program/results/ -d ../angora_results/$program/results_run2/ -d ../angora_results/$program/results_run3/ -d ../angora_results/$program/results_run4/ -d ../angora_results/$program/results_run5/ -o ../results/$program/raw_data.csv
 done
